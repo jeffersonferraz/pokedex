692be916c992f5968dd3fef27427547a12b3da28
ArturPlenty <artur.pophof@plentymarkets.com>
2023-03-10 11:49:17 +0100

    Slide bar gelöscht

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,6 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   ],
   imports: [
     BrowserModule,
-    MatSlideToggleModule,
     AppRoutingModule
   ],
   providers: [],
