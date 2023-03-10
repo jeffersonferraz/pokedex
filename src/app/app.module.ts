f679168888be6c29c266541beb5eefb39af99beb
jefferson <jefferson.ferraz@live.com>
2023-03-10 14:28:29 +0100

    update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import {MatCardModule} from '@angular/material/card';
-import { PokemonComponent } from './pokemon/pokemon.component';
-
+import { MatCardModule } from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 
 
--- src/app/pokemon.ts
+++ src/app/pokemon.ts
@@ -1,5 +1,5 @@
 export interface Pokemon {
     id: number;
     name: string;
-    sprites: string[];
+    //sprites: string[];
   }
