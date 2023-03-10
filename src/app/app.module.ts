6b7a08b60a2656d996ebd81f4856c6e91d6f7d03
ArturPlenty <artur.pophof@plentymarkets.com>
2023-03-10 11:26:29 +0100

    Angular Material Versuch 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    MatSlideToggleModule,
     AppRoutingModule
   ],
   providers: [],
